3bca1e7b801795ee183021feeee3ec9c21bebda8
Colin Beltz <colinbeltz@gmail.com>
2025-03-22 16:39:13 -0500

    Connected to Appwrite to be able to log users' searches in Appwrite.js file.

--- my-first-react-app/src/App.jsx
+++ my-first-react-app/src/App.jsx
@@ -3,6 +3,7 @@ import Search from './components/Search'
 import Spinner from './components/Spinner'
 import MovieCard from './components/MovieCard';
 import { useDebounce } from 'react-use';
+import { updateSearchCount } from './appwrite';
 
 const API_BASE_URL = 'https://api.themoviedb.org/3';
 
@@ -53,7 +54,13 @@ const fetchMovies = async (query = '') => {
       return;
     }
 
-    setMovieList(data.results || []); ;
+    setMovieList(data.results || []);
+
+if(query && data.results.length > 0) {
+  // Update the search count in Appwrite
+  await updateSearchCount(query, data.results[0]);
+}
+
   } catch (error) {
     console.error(`Error fetching movies: ${error}`);
     setErrorMessage('Failed to fetch movies. Please try again later.');
