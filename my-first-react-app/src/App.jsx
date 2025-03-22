6a7f01d56a91c7e14e0312eca3a0b205115100b2
Colin Beltz <colinbeltz@gmail.com>
2025-03-22 10:41:07 -0500

    Removed code from App.jsx. Installed Tailwind CSS.

--- my-first-react-app/src/App.jsx
+++ my-first-react-app/src/App.jsx
@@ -1,43 +1,12 @@
-import { useEffect, useState } from "react"
-
-// useState is used here because we want to keep track of the state of the count and the like button for each card.
-// useEffect is used here because we want to run some code when the component mounts and when the state of hasLiked changes.
-const Card = ( {title} ) => {
-  const [count, setCount] = useState(0);
-  const [hasLiked, setHasLiked] = useState(false);
-
-  useEffect(() => {
-    console.log(`${title} has been liked: ${hasLiked}`);
-  }, [hasLiked, title]);
-
-  // When the component mounts, this code will run. 
-  // useEffect(() => {
-  //   console.log('card rendered');
-  // }, []); 
-
-  return (  
-    // <div class="card" onClick={() => setCount((prevState) => prevState + 1)}>
-    // connditional reundering count || null Can also use count ? count : null Count will only show if it's greater than 0
-    <div class="card" onClick={() => setCount(count + 1)}>
-  <h2>{title} <br /> {count || null}</h2> 
-      
-      <button onClick={() => setHasLiked(!hasLiked)}>
-        {hasLiked ? '❤️' : "🤍"}
-      </button>
-    </div>
-  )
-}
+import React from 'react'
 
 const App = () => {
-
   return (
-  <div className="card-container">
-    <Card title="Star Wars" />
-    <Card title="Avatar" />
-    <Card title="The Lion King" />
-  </div>
-
-
+    <div>
+      <h1 class="text-3xl font-bold underline">
+        Hello world!
+      </h1>  
+    </div>
   )
 }
 
